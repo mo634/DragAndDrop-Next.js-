3d434341c86bd3346b11b61be9480c5fdca89367
MohamedMostafa <83365873+mo634@users.noreply.github.com>
2024-09-04 12:50:14 +0300

    store files uploaded in state

--- src/app/components/DropZone.tsx
+++ src/app/components/DropZone.tsx
@@ -1,14 +1,29 @@
 "use client"
-import { useCallback } from "react"
+import { useCallback, useState } from "react"
 import { useDropzone } from "react-dropzone"
 
 
 const DropZone = ({ className }: { className: string }) => {
+
+    const [files, setFiles] = useState<any[]>([]);
     const onDrop = useCallback((acceptedFiles: any) => {
-        console.log(acceptedFiles)
+        // If there are any accepted files
+        if (acceptedFiles.length > 0) {
+            // Update the state with the new files
+            setFiles(prev => [
+                // Keep the existing files
+                ...prev,
+                // Add the new files with a preview URL
+                ...acceptedFiles.map((file: any) =>
+                    Object.assign(file, {
+                        preview: URL.createObjectURL(file)
+                    })
+                )
+            ])
+        }
     }, [])
     const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
-
+    console.log("files", files)
     return (
         <div {...getRootProps({ className })}>
             <input {...getInputProps()} />
